edcdc4ab139b64d8a035449c2a49c3f207bc87dc
d4tk1dmVn <datkidmun@gmail.com>
2025-07-12 18:44:36 +0200

    Add shortcuts to scroll through snippets

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -28,6 +28,8 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
